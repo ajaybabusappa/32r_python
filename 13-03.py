daee43f5f97d848efa69f012a3f49836fd021ee7
AJAY BABU SAPPA <jysappa@gmail.com>
2025-03-13 11:12:46 +0530

    13 Mar classes

---
+++ 13-03.py
@@ -0,0 +1,84 @@
+
+# #Language - Grammer, Sentences, numbers, special characters
+# #Programming languages - Syntax, strings, numbers
+
+# num1 = 34
+# num2 = 0
+
+# #+, -, , /, //, *
+
+# #3.5, 3
+
+# #strings - 
+
+# happy_bday = 5
+# print('happy  day2')
+
+# #len
+# #operations -
+
+# str1 = 'abc'
+# str2 = 'bcd'
+
+# # print(str1 ** 2)
+
+
+# #Indexing - 
+
+
+# str1 = 'find your america'
+# print(len(str1))
+
+# print(str1[14])
+
+# #slicing
+
+# print(str1[2: 7])
+# print('check', str1[7: 2])
+
+# print(str1[5: 100])
+
+# print(str1[ : 5])
+# print(str1[5 : ])
+# print(str1[ : ])
+
+
+# #Negative indexing
+
+# str1 = 'adjkha asilhkasd'
+# str1[-len(str1)]
+
+# print(str1[-4: -9: -1])
+# print(str1[-9: -4])
+
+temp= {}
+
+str1 = 'adjkha asilhkasd'
+print(str1[2: 15])
+print(str1[2: 15: 1])
+
+
+#Data 
+#Datatype 
+
+#Different types of datatypes
+#Numeric datatypes - float, int, complex, boolean
+#Sequence datatypes - List, tuple, string, range
+#Set
+#Dict 
+#None
+
+#m + ni
+cmp1 = 5 + 6j
+cmp2 = 7 + 0j
+
+
+print(cmp1 + cmp2)
+
+#Boolean - True, False
+
+bool1 = True
+bool2 = False
+
+
+print(type(bool1))
